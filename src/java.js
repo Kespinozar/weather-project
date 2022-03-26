f22ca7951f1f4c4647ab3419a717654704cc8e4f
Kespinozar <karolynmishellv@gmail.com>
2022-03-26 16:14:00 -0400

    Removed the Celcius conversion

--- src/java.js
+++ src/java.js
@@ -65,7 +65,7 @@ forecastHTML= forecastHTML +
 function getForecast(coordinates) {
 
 let apiKey="d89de28f7e977585d9dbb24f44f2c66f";
-let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayForecast);
 }
@@ -109,7 +109,7 @@ getForecast(response.data.coord);
 function search(city){
 
 let apiKey="d89de28f7e977585d9dbb24f44f2c66f";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric `;
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial `;
 axios.get(apiUrl).then(displayTemperature);
     
 }
@@ -123,49 +123,13 @@ search(cityInput.value);
 
 }
 
-function displayFahrenheitTemp(event){
-event.preventDefault();
- 
-let temperatureElement= document.querySelector("#temperature");
-
-celsiusLink.classList.remove("active");
-fahrenheitLink.classList.add("active");
-let fahrenheiTemperature= ( celciusTemperature* 9)/5+32;
- temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
-
-}
-
-function displayCelsiusTemp(event){
-event.preventDefault();
-
-celsiusLink.classList.add("active");
-fahrenheitLink.classList.remove("active");
-
-let temperatureElement= document.querySelector("#temperature");
- temperatureElement.innerHTML = Math.round(celsiusTemperature);
-
-
-
-}
-
-
-
-
-
-
-let celsiusTemperature= null;
 
 
 
 let form= document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink= document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 
-
-let celsiusLink= document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 
 search("New York");
